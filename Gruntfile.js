ed53667a35e41011d1e20571e7650b623779d3ce
Romulo Santos <romulobr@gmail.com>
2013-08-31 15:27:29 -0300

    adds watch task to grunt, watches and lint all files on save.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,21 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       all:['Gruntfile.js', './*.js', 'lib/**/*.js', 'specs/**/*.js']
-    }
+    },
+    watch: {
+      scripts: {
+        files: ['Gruntfile.js', './*.js', 'lib/**/*.js', 'specs/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
 
