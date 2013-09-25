c504f88611aa00cd6be70f313fdee283cb4b3040
Romulo Santos <romulobr@gmail.com>
2013-09-25 02:23:20 -0300

    deletes IDE trash files, minor improvements to client and server network code.

--- public/js/index.js
+++ public/js/index.js
@@ -34,6 +34,18 @@ function createApp () {
                 .optionsToFields()
                 .bindEvents()
                 .fetchSocketUrl();
+            ko.bindingHandlers.fadeVisible = {
+                init: function(element, valueAccessor) {
+                    // Initially set the element to be instantly visible/hidden depending on the value
+                    var value = valueAccessor();
+                    $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+                },
+                update: function(element, valueAccessor) {
+                    // Whenever the value subsequently changes, slowly fade the element in or out
+                    var value = valueAccessor();
+                    ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+                }
+            };
                 ko.applyBindings(app.viewModel);
         },
         flipView: function () {
